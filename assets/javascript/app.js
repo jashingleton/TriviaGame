387756c2c4be4723fe61d4d504044b8a7eb3b57e
Julia Shingleton <jashingleton@gmail.com>
2018-02-24 17:11:48 -0500

    adding files

---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+$(document).ready( function() {
+
+var questions = [
+	{
+		question: "This Japanese dessert is made from sticky rice and ice cream, and comes in balls.",
+		choices: ["mochi", "anpan", "chinsuko", "anmitsu"],
+		correctAnswer: 0,
+	},
+	{
+		question: "What does the Italian word 'tiramisu' literally translate to?",
+		choices: ["sweet cake", "pick me up", "alcohol treat", "for the rich"],
+		correctAnswer: 1,
+	},
+	{
+		question: "Which of these ingredients is NOT in baklava?",
+		choices: ["honey", "chocolate", "phyllo dough", "nuts"],
+		correctAnswer: 1,
+	},
+	{
+		question: "This type of cake contains chocolate, whipped cream, and cherries and shares its name with a region in Germany:",
+		choices: ["Hamburg", "Berlin", "Black Forest", "Cologne"],
+		correctAnswer: 2,	
+	},
+	{
+		question: "How many times are biscotti biscuits baked?",
+		choices: ["four", "three", "two", "one"],
+		correctAnswer: 2,
+	},
+	{
+		question: "Which of these is NOT a flavor of Oreo that has been created?",
+		choices: ["watermelon", "candy corn", "fruit punch", "bubblegum"],
+		correctAnswer: 3,
+	},
+	{
+		question: "What does the 'pound' in pound cake refer to?",
+		choices: ["1 pound of flour, sugar, butter and eggs", "How much the cake weighs after it's cooked", "How much weight you gain for every piece you eat", "How much it used to cost to purchase at the bakery"],
+		correctAnswer: 0,	
+	},
+	{
+		question: "Which of these is not a type of ice cream?",
+		choices: ["sorbet", "faloodeh", "gelato", "kulfi"],
+		correctAnswer: 0,
+	},
+	{
+		question: "Which of these desserts does not traditionally get set on fire before being served?",
+		choices: ["bananas foster", "baked alaska", "cherries jubilee", "black forest cake"],
+		correctAnswer: 3,
+	},
+	{
+		question: "What type of cheese is typically used in New York cheesecakes?",
+		choices: ["cream cheese", "marscapone", "brie", "ricotta"],
+		correctAnswer: 0,
+	}];
+
+	var correct = 0; //correct answers
+	var incorrect = 0; //incorrect answers
+	var unanswered = 0; //timed out questions
+
+
+
+//click start to display questions and start timer
+
+
+}
